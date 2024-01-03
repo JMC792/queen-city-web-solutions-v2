bfecef51f1a06c81c4ccdbadc023e0e6ff893cae
Mauricio Chavez <mauriciochavez792@gmail.com>
2024-01-03 14:21:41 -0500

    restructured hero to be centered

--- app/(homepage)/Hero.js
+++ app/(homepage)/Hero.js
@@ -13,15 +13,15 @@ export default function Hero({title,subtitle,button}) {
                     {/* Horizontal Margin and Padding */}
                     <div className="pt-[13rem] md:pt-[15rem] lg:pt-[16rem]">
                         {/* Vertical Margin and Padding */}
-                        <div className="grid lg:grid-cols-2 pl-20">
+                        <div className="px-20">
 
 
-                            <div className="text-neutral-100 animate-slide-from-opacity">
+                            <div className="text-center text-neutral-100">
                                 {/* Text Wrapper */}
                                 <div className="space-y-10">
                                     
                                     {/* Hero Title */}
-                                    <div className="slide-from-opacity">
+                                    <div className="animate-slide-from-opacity">
                                         <div className="text-4xl lg:text-5xl 2xl:text-6xl">
                                             <div className="font-bold leading-tight">
                                                 {title}
@@ -31,14 +31,14 @@ export default function Hero({title,subtitle,button}) {
                                     
                                     
                                     {/* Hero Subtitle */}
-                                    <div className="">
+                                    <div className="animate-slide-from-opacity-2">
                                         <div className="text-lg">
                                             {subtitle}
                                         </div>
                                     </div>
                                     
                                     {/* Hero Button */}
-                                    <div>
+                                    <div className="animate-slide-from-opacity-3">
                                         <Link href="/contact" >
                                             <button type="button" className="bg-orange-800 rounded-lg md:text-[20px] leading-[25px] px-7 py-4 mt-4 w-[14rem] uppercase">
                                                 {button}
