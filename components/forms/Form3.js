e0f2315975967648b015437fb3cae8d64160ebe6
Mauricio Chavez <mauriciochavez792@gmail.com>
2024-03-08 20:29:51 -0500

    wip

--- components/forms/Form3.js
+++ components/forms/Form3.js
@@ -98,12 +98,26 @@ export default function Form3(props) {
           </div>
 
           {/* Form */}
-          <form data-netlify="true" method="POST" name="contact" netlify-honeypot="bot-field" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+          <form 
+            name="contact" 
+            method="POST" 
+            data-netlify="true" 
+            netlify-honeypot="bot-field" 
+            className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
+          >
             <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
               <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+
+                <input type="hidden" name="form-name" value="contact" />
+
+                <p class="hidden">
+                    <label>
+                    Dont fill this out if youre human: <input name="bot-field" />
+                    </label>
+                </p>
                 
                 {/* First Name */}
-                <p>
+                <div>
                   <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
                     First name
                   </label>
@@ -116,10 +130,10 @@ export default function Form3(props) {
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
-                </p>
+                </div>
 
                 {/* Last Name */}
-                <p>
+                <div>
                   <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
                     Last name
                   </label>
@@ -132,10 +146,10 @@ export default function Form3(props) {
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
-                </p>
+                </div>
 
                 {/* Email */}
-                <p className="sm:col-span-2">
+                <div className="sm:col-span-2">
                   <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
                     Email
                   </label>
@@ -148,10 +162,10 @@ export default function Form3(props) {
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
-                </p>
+                </div>
 
                 {/* Phone Number */}
-                <p className="sm:col-span-2">
+                <div className="sm:col-span-2">
                   <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
                     Phone number
                   </label>
@@ -164,10 +178,10 @@ export default function Form3(props) {
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
-                </p>
+                </div>
 
                 {/* Street Address */}
-                <p className="sm:col-span-2">
+                <div className="sm:col-span-2">
                   <label htmlFor="street-address" className="block text-sm font-semibold leading-6 text-gray-900">
                     Street Address
                   </label>
@@ -180,10 +194,10 @@ export default function Form3(props) {
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
-                </p>
+                </div>
 
                 {/* City */}
-                <p>
+                <div>
                   <label htmlFor="city" className="block text-sm font-semibold leading-6 text-gray-900">
                     City
                   </label>
@@ -196,10 +210,10 @@ export default function Form3(props) {
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
-                </p>
+                </div>
 
                 {/* State */}
-                <p>
+                <div>
                   <label htmlFor="state" className="block text-sm font-semibold leading-6 text-gray-900">
                     State
                   </label>
@@ -212,11 +226,10 @@ export default function Form3(props) {
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
-                </p>
-                
+                </div>
                 
                 {/* Message */}
-                <p className="sm:col-span-2">
+                <div className="sm:col-span-2">
                   <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
                     Description of Issue & Services Needed
                   </label>
@@ -229,19 +242,19 @@ export default function Form3(props) {
                       defaultValue={''}
                     />
                   </div>
-                </p>
+                </div>
 
               </div>
 
               {/* Button */}
-              <p className="mt-8 flex justify-end">
+              <div className="mt-8 flex justify-end">
                 <button
                   type="submit"
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Send message
                 </button>
-              </p>
+              </div>
               
             </div>
           </form>
