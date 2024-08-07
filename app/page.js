1209d064535cce35a4b07f88b09f27cf50f4ad81
Mauricio Chavez <mauriciochavez792@gmail.com>
2024-08-07 17:03:28 -0400

    update node

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import Hero from "@components/hero/Hero";
 import SimpleCentered from "@components/testimonials/SimpleCentered";
 //Components
 import OffsetList from "@components/features/OffsetList.js";
-import Form3 from "@components/forms/Form3.js";
 import Stat from "@components/stats/Stat.js";
 import CTA from "@components/CTA/CTA1.js";
 import FAQ1 from "@components/FAQ/FAQ1.js";
--- components/features/ProductScreenshot.js
+++ components/features/ProductScreenshot.js
@@ -5,17 +5,17 @@ const features = [
   {
     name: 'Swift and Efficient Service',
     description:
-      'Prompt response times and efficient handling of your immigration matters. Valentin Law, LLC itself on its speed and effectiveness, ensuring your needs are addressed in a timely manner while maintaining the highest standards of quality.',
+      'Prompt response times and efficient handling of your IT needs. Queen City IT Solutions prides itself on its speed and effectiveness, ensuring your IT challenges are addressed swiftly while maintaining the highest standards of quality.',
     icon: CheckCircleIcon,
   },
   {
     name: 'Expertise and Accuracy',
-    description: 'Valentin Law, LLC`s expertise in immigration law ensures accurate and precise handling of your case. You can trust that every detail will be meticulously addressed, resulting in optimal outcomes for your legal matters.',
+    description: 'Our expertise in IT services and consultation ensures accurate and precise handling of your projects. You can trust that every detail will be meticulously addressed, resulting in optimal outcomes for your IT infrastructure and security needs.',
     icon: CheckCircleIcon,
   },
   {
     name: 'Dedicated to Client Satisfaction',
-    description: 'Client satisfaction drives everything. From the initial consultation to the resolution of your immigration issues, your satisfaction is most important. Trust Valentin Law, LLC for an excellent service experience.',
+    description: 'Client satisfaction drives everything we do. From the initial consultation to the resolution of your IT issues, your satisfaction is our top priority. Trust Queen City IT Solutions for an excellent service experience.',
     icon: CheckCircleIcon,
   },
 ]
@@ -30,13 +30,13 @@ export default function ProductScreenshot(props) {
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
               <h2 className="text-base font-semibold leading-7 text-primary">
-                {props.heading}
+                About Us
               </h2>
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                {props.title}
+                Our Story and Mission
               </p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                {props.paragraph}
+                Queen City IT Solutions was founded in Charlotte with a mission to provide top-tier IT services and consultation. We are dedicated to enhancing businesses' IT infrastructure and security nationwide.
               </p>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {features.map((feature) => (
@@ -53,12 +53,10 @@ export default function ProductScreenshot(props) {
           </div>
 
           {/* Image */}
-          <Image
-            src={props.image}
-            alt="Product screenshot"
-            className="order-1 w-[48rem] lg:max-w-none rounded-xl ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-            width={2432}
-            height={1442}
+          <img
+            alt=""
+            src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
           />
         </div>
       </div>
