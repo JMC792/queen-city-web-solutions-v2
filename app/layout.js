45c97c8f676ac1deccca9d5faca99bb209766feb
Mauricio Chavez <mauriciochavez792@gmail.com>
2024-08-13 18:06:41 -0400

    changed phoone number

--- app/layout.js
+++ app/layout.js
@@ -25,18 +25,34 @@ export const metadata = {
 
 const inter =Inter({subsets:['latin']})
 
-export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children
-}) {
+export default function RootLayout({children}){
+  
+  const layout = {
+    title: "Valentin Law",
+    description: "Valentin Law LLC, based in Atlanta, GA, specializes in immigration law. We offer expert guidance on visas, green cards, and citizenship with a personal touch. Navigate your immigration journey with confidence.",
+    applicationName: "Vapor X Crawlspaces",
+    creator: "Jesus Chavez",
+    publisher: "Jesus Chavez",
+    number: "(980)-392-7215",
+    email: "info@queencitywebsolutions.com",
+    address: "1422 S Tryon St",
+    city: "Charlotte, NC 28203",
+    sitemap: {
+        home: "home",
+        about: "about",
+        link3: "portfolio",
+        blog: "blog",
+        contact: "contact",
+        service: "service"
+    }
+  };
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <Navbar
-          link1 = {layout.sitemap.home} //Home
           link2 = {layout.sitemap.about} //About
-          link3 = {layout.sitemap.portfolio} //Portfolio
+          link3 = {layout.sitemap.link3} //Portfolio
           link4 = {layout.sitemap.contact} //Contact
           link5 = {layout.sitemap.service} //Service
           link6 = {layout.sitemap.blog} //Blog
@@ -44,34 +60,7 @@ export default function RootLayout({
           logo = {logo}
         />
           {children}
-        <Footer
-          overview = {[
-            layout.title, 
-            layout.description,
-            logo
-          ]}
-          internal={[
-            layout.sitemap.home, //Home
-            layout.sitemap.about, //About
-            //layout.sitemap.portfolio, //Portfolio
-            layout.sitemap.contact, //Contact
-            layout.sitemap.service, //Service
-          ]}
-          outgoing={[
-            [footer.socialMedia.media1.title, footer.socialMedia.media1.link], //Instagram
-            
-            [footer.socialMedia.media3.title, footer.socialMedia.media3.link], //Facebook
-          ]}
-          contact= {[
-            [footer.contact.phone.title, footer.contact.phone.link, call, "call"],
-            [footer.contact.email.title, footer.contact.email.link, email, "email"],
-          ]}
-          trademark={[
-            footer.trademark.year,
-            footer.trademark.company,
-            footer.trademark.rights
-          ]}
-        />
+        <Footer/>
       </body>
     </html>
   );
